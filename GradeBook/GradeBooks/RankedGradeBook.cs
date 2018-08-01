b1dede78963d34da71b7653bd99d2cd9c03403be
Jeremy Calahan <jcalahan@peerlessproducts.com>
2018-08-01 08:11:23 -0500

    Letter grade checking based on what percentage of the class

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,40 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.20);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            //creates a list (grades) to hold the grades of the class. Sorts the Students by the AverageGrade, puts them in a list and then
+            //selects those grades and sets grades equal to the list.
+
+
+            if (grades[threshold - 1] <= averageGrade)
+            {
+                return 'A';
+
+            }
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(threshold * 4)] - 1 <= averageGrade)
+            {
+                return 'D';
+            }
+            else return 'F';
+                //return base.GetLetterGrade(averageGrade);
         }
     }
 }
